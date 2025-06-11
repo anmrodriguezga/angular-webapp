03875f43c22f9502e8af1a640325d3c8adf89f9c
Andres Mauricio Rodriguez Garzon <anmrodriguezga@unal.edu.co>
2025-06-11 08:31:07 -0500

    Clase 3: Creacion de componentes y rutas

--- todoapp/src/app/app.routes.ts
+++ todoapp/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Labs } from './pages/labs/labs';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+    },
+    {
+        path: 'labs',
+        component: Labs,
+    }
+];
--- todoapp/src/app/app.ts
+++ todoapp/src/app/app.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected welcome = 'Hola';
-  protected tasks = [
+  title = 'todo-app';
+  welcome = 'Hola';
+  tasks = [
     'Instalar el Angular CLI',
     'Crear un nuevo proyecto',
     'Crear un componente'
-  ]
+  ];
 }
