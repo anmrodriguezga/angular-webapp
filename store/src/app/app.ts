9e98c2d68cfe60228262c270547a0249f1c32199
Andres Mauricio Rodriguez Garzon <anmrodriguezga@unal.edu.co>
2025-06-17 18:56:54 -0500

    Clase 43: Creacion de headers en paginas web

--- store/src/app/app.ts
+++ store/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './domains/shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: '<router-outlet />'
+  imports: [RouterOutlet, Header],
+  templateUrl: './app.html'
 })
 export class App {
   protected title = 'store';
--- store/src/app/domains/products/pages/list/list.ts
+++ store/src/app/domains/products/pages/list/list.ts
@@ -18,7 +18,10 @@ export class List {
       { id: 2, title: 'Product 2', price: 200, image: 'https://picsum.photos/640/640?r=24', creationDate: new Date() },
       { id: 3, title: 'Product 3', price: 300, image: 'https://picsum.photos/640/640?r=25', creationDate: new Date() },
       { id: 4, title: 'Product 4', price: 400, image: 'https://picsum.photos/640/640?r=26', creationDate: new Date() },
-      { id: 5, title: 'Product 5', price: 500, image: 'https://picsum.photos/640/640?r=27', creationDate: new Date() }
+      { id: 5, title: 'Product 5', price: 500, image: 'https://picsum.photos/640/640?r=27', creationDate: new Date() },
+      { id: 6, title: 'Product 6', price: 600, image: 'https://picsum.photos/640/640?r=28', creationDate: new Date() },
+      { id: 7, title: 'Product 7', price: 700, image: 'https://picsum.photos/640/640?r=29', creationDate: new Date() },
+      { id: 8, title: 'Product 8', price: 800, image: 'https://picsum.photos/640/640?r=30', creationDate: new Date() }
     ];
     this.products.set(initialProducts);
   }
