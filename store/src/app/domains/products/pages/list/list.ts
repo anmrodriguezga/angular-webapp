17fcc12f00d869dee42b3c2dc42cd377a8a5c815
Andres Mauricio Rodriguez Garzon <anmrodriguezga@unal.edu.co>
2025-06-17 09:39:35 -0500

    Clase 30: Componentes reutilizables, creacion y maquetado de productos

--- store/src/app/domains/products/pages/list/list.ts
+++ store/src/app/domains/products/pages/list/list.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Product } from '../../components/product/product';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [Product],
   templateUrl: './list.html',
   styleUrl: './list.css'
 })
 export class List {
-
+  
 }
