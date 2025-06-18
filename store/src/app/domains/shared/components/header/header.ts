c711c19917ce570290bc66f9804b56242c6dcab9
Andres Mauricio Rodriguez Garzon <anmrodriguezga@unal.edu.co>
2025-06-18 15:26:03 -0500

    Clase 46: Gestion de carrito de compras con ngOnChanges

--- store/src/app/domains/shared/components/header/header.ts
+++ store/src/app/domains/shared/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
 import { ProductModel } from '../../models/product.model';
 
 @Component({
@@ -12,9 +12,21 @@ export class Header {
   @Input({ required: true }) cart: ProductModel[] = [];
 
   hideSidebar = signal(true);
+  total = signal(0);
+
+  ngOnChanges(changes: SimpleChanges) {
+    const cart = changes['cart'];
+    if (cart) {
+      this.total.update(() => this.calcularTotal());
+    }
+  }
 
   toggleSidebar() {
     this.hideSidebar.update(prevState => !prevState);
+  }
+
+  calcularTotal() {
+    return this.cart.reduce((total, product) => total + product.price, 0);
   }
 
 }
