91ffa1de89458d378eacda423ff736a3f8720b2d
Andres Mauricio Rodriguez Garzon <anmrodriguezga@unal.edu.co>
2025-06-19 15:45:30 -0500

    Clase 59: Implementacion de layouts compartidos para vistas anidadas

--- store/src/app/app.routes.ts
+++ store/src/app/app.routes.ts
@@ -3,15 +3,22 @@ import { Routes } from '@angular/router';
 import { List } from './domains/products/pages/list/list'
 import { About } from './domains/info/pages/about/about';
 import { NotFound } from './domains/info/pages/not-found/not-found';
+import { Layout } from '@shared/components/layout/layout';
 
 export const routes: Routes = [
     {
         path: '',
-        component: List,
-    },
-    {
-        path: 'about',
-        component: About,
+        component: Layout,
+        children: [
+            {
+                path: '',
+                component: List,
+            },
+            {
+                path: 'about',
+                component: About,
+            },
+        ]
     },
     {
         path: '**',
