59b59c459f34ef3d56ef0d3ff09defab196c82b3
Andres Mauricio Rodriguez Garzon <anmrodriguezga@unal.edu.co>
2025-06-20 16:57:44 -0500

    Clase 65: Filtrado de productos por categoria

--- store/src/app/domains/products/pages/list/list.ts
+++ store/src/app/domains/products/pages/list/list.ts
@@ -1,7 +1,9 @@
 import { Component, signal, inject } from '@angular/core';
 import { Product } from '@products/components/product/product';
+import { CategoryModel } from '@shared/models/category';
 import { ProductModel } from '@shared/models/product';
 import { CartService } from '@shared/services/cart';
+import { CategoryService } from '@shared/services/category';
 import { ProductService } from '@shared/services/product';
 
 @Component({
@@ -14,10 +16,21 @@ export class List {
 
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
 
   products = signal<ProductModel[]>([]);
+  categories = signal<CategoryModel[]>([]);
 
   ngOnInit() {
+    this.getProducts();
+    this.getCategories();
+  }
+
+  addToCart(product: ProductModel) {
+    this.cartService.addToCart(product);
+  }
+
+  private getProducts() {
     this.productService.getProducts().subscribe({
       next: (products) => {
         this.products.set(products);
@@ -28,7 +41,15 @@ export class List {
     });
   }
 
-  addToCart(product: ProductModel) {
-    this.cartService.addToCart(product);
+  private getCategories() {
+    this.categoryService.getCategories().subscribe({
+      next: (categories) => {
+        this.categories.set(categories);
+      },
+      error: (error) => {
+        console.error('Error fetching categories:', error);
+      }
+    });
   }
+
 }
---
+++ store/src/app/domains/shared/services/category.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { CategoryModel } from '@shared/models/category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private http = inject(HttpClient);
+
+  constructor() { }
+
+  getCategories() {
+    return this.http.get<CategoryModel[]>('https://api.escuelajs.co/api/v1/categories');
+  }
+}
