13fbf5e13b39dbbfea4e27b354ce5b035722cbf2
Andres Mauricio Rodriguez Garzon <anmrodriguezga@unal.edu.co>
2025-06-25 16:45:15 -0500

    Clase 6: Ciclo de vida de componentes, constructor a onDestroy

--- my-store/src/app/components/img/img.ts
+++ my-store/src/app/components/img/img.ts
@@ -13,6 +13,36 @@ export class Img {
   @Output() loaded = new EventEmitter<string>();
   imageDefault: string = './assets/default-image.jpg';
 
+  constructor() {
+    // before render
+    // avoid async -- executed once
+    console.log('Constructor', 'img:', this.img);
+  }
+
+  ngOnChanges() {
+    // before-during render
+    // input changes - executed every update
+    console.log('ngOnChanges', 'img:', this.img);
+  }
+
+  ngOnInit() {
+    // before render
+    // allow async -- executed once
+    console.log('ngOnInit', 'img:', this.img);
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // children handler
+    console.log('ngAfterViewInit', 'img:', this.img);
+  }
+
+  ngOnDestroy() {
+    // on delete
+    // avoid async -- executed once
+    console.log('ngOnDestroy', 'img:', this.img);
+  }
+
   imgError() {
     this.img = this.imageDefault;
   }
